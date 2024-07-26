699814a85480d7a9569bc86dce678d77275c2c61
Michael Brookins <michael.brookins@rfsmart.com>
2024-07-26 15:00:34 -0400

    GET, Manipulate, Reverse, Log last 5, create artifact

--- tests/API/api_2_tests.spec.js
+++ tests/API/api_2_tests.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
-const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 
 //API URL
 const baseURL = "https://jsonplaceholder.typicode.com";
@@ -36,6 +36,25 @@ test('API POST Request', async ({ request }) => {
   console.log(jsonResponse);
   });
 
+//Test Case 3 - Use API returned results in a workflow
 
-//Test Case 3 - Workflow usuage - Get Data Manipulation Reverse Data and Save as Artifact
 
+//Test Case 4 - Get Data Manipulation Reverse Data and Save as Artifact
+test('GET API Response', async ({ request }) => {
+  const response = await request.get(`${baseURL}/posts`, {
+    ignoreHTTPSErrors: true,
+  });
+  expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  //Data Manipulation
+  //Reverse Order
+  const reversedPosts = responseBody.reverse();
+  // console.log('Reversed posts:', reversedPosts);
+   // Select Last 5 Posts
+  const last5Posts = responseBody.slice(-5);
+  console.log('Last 5 posts:', last5Posts);
+  //Save last 5 posts as an artifact
+  const artifactPath = path.resolve(__dirname, 'last5Posts.json');
+  fs.writeFileSync(artifactPath, JSON.stringify(last5Posts, null, 2));
+  console.log(`Last 5 posts saved to ${artifactPath}`);
+});
