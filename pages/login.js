f2c0ead23fea948d68d566141f91558a539e140f
Michael Brookins <michael.brookins@rfsmart.com>
2024-08-01 13:19:49 -0400

    login verification

--- pages/login.js
+++ pages/login.js
@@ -2,15 +2,13 @@ export class LoginPage {
 
     constructor(page) {
         this.page = page
-        this.username_textbox = page.getByLabel('Username')
-        this.password_textbox = page.getByLabel('Password')
+        this.username_textbox = page.getByPlaceholder('Username')
+        this.password_textbox = page.getByPlaceholder('Password')
         this.login_button = page.getByRole('button', {name: 'Login'})
     }
-
     async gotoLoginPage() {
         await this.page.goto('https://www.saucedemo.com/')
     }
-
     async login(username, passowrd) {
         await this.username_textbox.fill(username)
         await this.password_textbox.fill(passowrd)
--- tests/UI/login.spec.js
+++ tests/UI/login.spec.js
@@ -1,19 +1,10 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/login';
+import { LoginPage } from '../../pages/login';
 
-// Login Verification Test
+// Login Verification 
 test('login', async ({ page }) => {
   const Login = new LoginPage(page);
-
-  // Navigate to the login page
   await Login.gotoLoginPage();
-
-  // Perform login action
   await Login.login('standard_user', 'secret_sauce');
-
-  // Verify successful login by checking the URL
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-
-  // Verify a unique element that appears after login
-  await expect(page.locator('.inventory_list')).toBeVisible(); // Update with an actual unique selector
 });
